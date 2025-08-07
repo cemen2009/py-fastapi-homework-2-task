7a75cc404ac24b39adddbea2ff913fa5fa3efddf
cemen <frodo.work@gmail.com>
2025-08-07 17:45:14 +0300

    basic schemas structure

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -6,8 +6,20 @@ from sqlalchemy.orm import joinedload
 
 from database import get_db, MovieModel
 from database.models import CountryModel, GenreModel, ActorModel, LanguageModel
+from schemas import MovieListResponseSchema
 
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get(
+    "/movies/",
+    response_model=MovieListResponseSchema,
+    responses={404: {"detail": "No movies found"}}
+)
+async def get_movies(db: AsyncSession = Depends(get_db)):
+    per_page = 10
+    page = 1
+    result = await db.execute(select(MovieModel).limit(per_page).offset(page * per_page))
+    movies = result.scalars()
+    return movies
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,22 @@
-# Write your code here
+from datetime import date
+
+from pydantic import HttpUrl, BaseModel
+
+
+# TODO: create enum for status of the movie
+
+
+class MovieDetailSchema(BaseModel):
+    id: int
+    name: str
+    date: date
+    score: float
+    overview: str
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: list[MovieDetailSchema]
+    prev_page: HttpUrl | None
+    next_page: HttpUrl | None
+    total_pages: int
+    total_items: int
